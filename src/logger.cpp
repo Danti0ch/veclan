4ba343fcdb71ed16877076026654e4b092cc8273
Danti0ch <ivanov.mikhail@phystech.edu>
2022-03-30 02:48:49 +0300

    first version of logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -6,38 +6,233 @@
 #include <string.h>
 #include <stdlib.h>
 
-static FILE* log_file = NULL;
+static FILE* dlog_file       = NULL;
+static FILE* clog_file       = NULL;
+
+//static FILE* dlogger_stream = stdout;         // TODO: распарсить эти опции через ключи ком строки
+
+// can i fix global struct?
+static log_location DCUR_LOC = {
+        "", "", 0
+};
 
 //----------------------LOCAL-FUNCTIONS-DECLARATION-----------------------//
 
 void get_log_name(char* str);
+void log_splitter(COMP_STAGE cur_stage, void* log_data){
+void clog_init(CDUMP_OPTION mode);
+void dlog_init();
+void debug_log(DLOG_TYPE type, const char* string, err_loc location);
 
 //----------------------PUBLIC-FUNCTIONS-DEFINITIONS----------------------//
 
-int LogInit(){
+int LogInit(CDUMP_OPTION mode, const char* path_to_clogs){
 
-	char name[MAX_LOG_NAME_LEN] = "";
-	get_log_name(name);
-	log_file = fopen(name, "w");
-
-	if(log_file == NULL){
-        printf("Unable to create log file on way: %s%s\n", LOG_LOCATION, name);
-        return 1;
-    }
-
-	LOG(INFO, "Logging initiated");
+    dlog_init();
+    clog_init(mode, path_to_clogs);
 
 	return;
 }
 //--------------------------------------------//
 
+
 void LogClose(){
 
-	assert(log_file != NULL);
-	ConsoleLog(LOG_TYPE::INFO, "Logging closed");
-	fclose(log_file);
+	DLOG(INFO, "Logging closed");
+	
+    #if DLOG_TO_FILE_ENABLED
+    assert(dlog_file != NULL);
+    fclose(dlog_file);
+    #endif  // DLOG_TO_FILE_ENABLED
+	
+    return;
+}
+//--------------------------------------------//
 
-	return;
+//----------------------LOCAL-FUNCTIONS-DEFINITIONS----------------------//
+
+void get_log_name(char* buf, const char* log_location){
+
+    assert(buf != NULL);
+
+	strcpy(buf, log_location);
+
+    time_t rawtime = time(NULL);
+    struct tm *ptm = localtime(&rawtime);
+
+    strftime(buf + strlen(buf), MAX_LOG_NAME_LEN, "%H-%M-%S(%d.%m.%Y)", ptm);
+
+    return;
+}
+//--------------------------------------------//
+
+
+// https://habr.com/ru/post/138150/
+// TODO: void should work
+void* dlog_wrapper(log_location location){
+
+    DCUR_LINE = location.n_line;
+    DCUR_FILE_NAME = location.file_name;
+    DCUR_FUNC_NAME = location.func_name;
+    
+    return &debug_log;
+}
+//--------------------------------------------//
+
+void* edlog_wrapper(log_location location){
+
+    DCUR_LINE = location.n_line;
+    DCUR_FILE_NAME = location.file_name;
+    DCUR_FUNC_NAME = location.func_name;
+    
+    return &debug_log_error;
+}
+//--------------------------------------------//
+
+// больше выделения для ошибок и ворнингов в файле???
+void debug_log(DLOG_TYPE type, const char* string, ...){
+
+    assert(string != NULL);
+    
+    time_t rawtime = time(NULL);
+    struct tm *ptm = localtime(&rawtime);
+
+    char time_str[20] = "";
+    strftime(time_str, 16, "%H:%M:%S", ptm);
+
+	va_list args;
+	va_start(args, str);
+
+    #if DLOG_TO_FILE_ENABLED
+    switch(type){
+        case INFO:{
+            fprintf(dlog_file, "[ERROR] %s--%s--%s<%u>:: \n",
+                time_str, DCUR_LOC.file_name, DCUR_LOC.func_name, DCUR_LOC.n_line);
+            break;
+        }
+        case INFO:{
+            fprintf(dlog_file, "[ERROR] %s--%s--%s<%u>:: \n",
+                time_str, DCUR_LOC.file_name, DCUR_LOC.func_name, DCUR_LOC.n_line);
+            break;
+        }
+        default:{
+            fprintf(dlog_file, "[UNDEFINIED] %s--%s--%s<%u>:: \n",
+                time_str, DCUR_LOC.file_name, DCUR_LOC.func_name, DCUR_LOC.n_line);
+            break;
+        }
+    }
+    
+    vfprintf(dlog_file, string, args);
+	fprintf(dlog_file, "\n");
+	
+    #endif  //DLOG_TO_FILE_ENABLED
+
+    #if DLOG_TO_CONSOLE_ENABLED
+
+    switch(type){
+        case INFO:{
+            fprintf(dlog_file, "%s[ERROR] %s--%s--%s<%u>:: \n",
+                WARNING_FONT, time_str, DCUR_LOC.file_name, DCUR_LOC.func_name, DCUR_LOC.n_line);
+            break;
+        }
+        case INFO:{
+            fprintf(dlog_file, "%s[ERROR] %s--%s--%s<%u>:: \n",
+                INFO_FONT, time_str, DCUR_LOC.file_name, DCUR_LOC.func_name, DCUR_LOC.n_line);
+            break;
+        }
+        default:{
+            fprintf(dlog_file, "%s[UNDEFINIED] %s--%s--%s<%u>:: \n",
+                INVALID_LOG_FONT, time_str, DCUR_LOC.file_name, DCUR_LOC.func_name, DCUR_LOC.n_line);
+            break;
+        }
+    }
+    
+    vprintf(string, args);
+	printf("%s\n", TERM_RESET);
+
+    #endif  // DLOG_TO_CONSOLE_ENABLED
+
+	va_end(args);
+
+    return;
+}
+//--------------------------------------------//
+
+void debug_log_error(ERROR_CODE err_code, const char* string, ...){
+
+    assert(string != NULL);
+    
+    time_t rawtime = time(NULL);
+    struct tm *ptm = localtime(&rawtime);
+
+    char time_str[20] = "";
+    strftime(time_str, 16, "%H:%M:%S", ptm);
+
+	va_list args;
+	va_start(args, str);
+
+    fprintf(dlog_file, "[ERROR] %s--%s--%s<%u>:: \n",
+        time_str, DCUR_LOC.file_name, DCUR_LOC.func_name, DCUR_LOC.n_line);
+
+    vfprintf(dlog_file, string, args);
+	fprintf(dlog_file, "\n");
+	
+    #endif  //DLOG_TO_FILE_ENABLED
+
+    #if DLOG_TO_CONSOLE_ENABLED
+
+    fprintf(dlog_file, "%s[ERROR] %s--%s--%s<%u>:: \n",
+        ERROR_FONT, time_str, DCUR_LOC.file_name, DCUR_LOC.func_name, DCUR_LOC.n_line);
+
+    vprintf(string, args);
+	printf("%s\n", TERM_RESET);
+
+    #endif  // DLOG_TO_CONSOLE_ENABLED
+
+	va_end(args);
+
+    LogClose();
+    exit(err_code);
+    
+    return;
+}
+//--------------------------------------------//
+
+void clog_init(CDUMP_OPTION mode, const char* path){
+
+    if(mode == CDUMP_OPTION::FILE || mode == CDUMP_OPTION::BOTH){
+        char name[MAX_LOG_NAME_LEN] = "";
+        get_log_name(name, path);
+        сlog_file = fopen(name, "w");
+
+        if(clog_file == NULL){
+            EDLOG(CREATING_FILE, "Unable to create log file on way: %s%s\n", LOG_LOCATION, name);
+        }
+    }
+
+    DLOG(INFO, "Compiler logging initiated");
+
+    return;
+}
+//--------------------------------------------//
+
+int dlog_init(){
+
+    #if DLOG_TO_FILE_ENABLED
+    
+    char name[MAX_LOG_NAME_LEN] = "";
+    get_log_name(name, LOG_LOCATION);
+    dlog_file = fopen(name, "w");
+
+    if(dlog_file == NULL){
+        printf("Unable to create log file on way: %s%s\n", LOG_LOCATION, name);
+        return 1;
+    }
+    #endif //DLOG_TO_FILE_ENABLED
+
+    DLOG(INFO, "Logging initiated");
+
+    return;
 }
 //--------------------------------------------//
 
@@ -70,73 +265,5 @@ void log_splitter(COMP_STAGE cur_stage, void* log_data){
             DLOG()
         }
     }
-}
-void LOG(LOG_TYPE type, const char* str, ...){
-
-    time_t rawtime = time(NULL);
-    struct tm *ptm = localtime(&rawtime);
-
-    char time_str[20] = "";
-    strftime(time_str, 16, "%H:%M:%S", ptm);
-
-	va_list args;
-	va_start(args, str);
-
-	if(type == LOG_TYPE::ERROR){
-		fprintf(log_file, "[ERROR] %s: ", time_str);
-	}
-	else if(type == LOG_TYPE::INFO){
-		fprintf(log_file, "[INFO] %s: ", time_str);
-	}
-	else if(type == LOG_TYPE::WARNING){
-		fprintf(log_file, "[WARNING] %s: ", time_str);
-	}
-	vfprintf(log_file, str, args);
-	fprintf(log_file, "\n");
-		
-	if(type == LOG_TYPE::ERROR){
-		printf("%s[ERROR] %s: ", ERROR_FONT, time_str);
-	}
-	else if(type == LOG_TYPE::INFO){
-		printf("%s[INFO] %s: ", WARNING_FONT, time_str);
-	}
-	else if(type == LOG_TYPE::WARNING){
-		printf("%s[WARNING] %s: ", INFO_FONT, time_str);
-	}
-	
-	vprintf(str, args);
-	printf("%s\n", TERM_RESET);
-
-	va_end(args);
-
-	if(type == LOG_TYPE::ERROR){
-		LogClose();
-		exit(0);
-	}
-
-	return;
-}
-
-void DebugLog(DLOG_TYPE type, const char* string, err_loc location){
-
-    assert(string != NULL);
-
-    
-    return;
-}
-//----------------------LOCAL-FUNCTIONS-DEFINITIONS----------------------//
-
-void get_log_name(char* buf){
-
-    assert(buf != NULL);
-
-	strcpy(buf, LOG_LOCATION);
-
-    time_t rawtime = time(NULL);
-    struct tm *ptm = localtime(&rawtime);
-
-    strftime(buf + strlen(buf), MAX_LOG_NAME_LEN, "%H-%M-%S(%d.%m.%Y)", ptm);
-
-    return;
 }
 //--------------------------------------------//
--- src/logger.h
+++ src/logger.h
@@ -1,7 +1,14 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-enum COMP_STAGES{
+enum ERROR_CODE{
+
+    OK,
+    ALLOC_MEM,
+    CREATING_FILE,
+};
+
+enum COMP_STAGE{
 
     INPUT_HANDLER,
     LEXER,
@@ -10,17 +17,23 @@ enum COMP_STAGES{
     ASM
 };
 
-struct err_loc{
+enum class CDUMP_OPTION{
 
-    char* file_name;
-    char* func_name;
-    uint  n_line;
+    CONSOLE,
+    FILE,
+    BOTH    
+};
+
+struct log_location{
+
+    char    file_name[1 << 8];
+    char    func_name[1 << 8];
+    uint    n_line;
 };
 
 enum DLOG_TYPE{
     INFO,
     WARNING,
-    ERROR
 };
 
 // for debugging errors
@@ -34,14 +47,36 @@ const int  MAX_LOG_NAME_LEN  = 2 << 8;
 const char LOG_LOCATION[32]  = "../logs/";  // TODO: make config for customise this option
 const int  MAX_LOG_STR_LEN   = 2 << 10;
 
-// TODO: make/find way to implement possibility to right specification strings
-#define DLOG(type, string)               \\
-{                                         \\
-                                           \\
-    DebugLog(type, string, {__FILE__,       \\
-                            __FUNCTION__,    \\
-                            __LINE__});       \\
-}
+// https://habr.com/ru/post/138150/
+#define DLOG   dlog_wrapper({__FILE__, __FUNCTION__, __LINE__})
+#define EDLOG edlog_wrapper({__FILE__, __FUNCTION__, __LINE__})
+
+#ifndef DLOG_MODE
+#define DLOG_MODE 0/**
+					 * DLOG_MODE = CDUMP_OPTION::CONSOLE, - debug logging only to console
+                     * DLOG_MODE = CDUMP_OPTION::FILE,    - debug logging only to file     
+                     * DLOG_MODE = CDUMP_OPTION::BOTH,    - debug logging to file and console
+					 */
+#endif
+
+#if DLOG_MODE == 0 || DLOG_MODE == 2
+    #define DLOG_TO_CONSOLE_ENABLED 1
+#else
+    #define DLOG_TO_CONSOLE_ENABLED 0
+#endif
+
+#if DLOG_MODE == 1 || DLOG_MODE == 2
+    #define DLOG_TO_FILE_ENABLED 1
+#else
+    #define DLOG_TO_FILE_ENABLED 0
+#endif
+
+#define TERM_RESET   	  "\033[0m"
+#define ERROR_FONT        "\033[1;31m"
+#define WARNING_FONT  	  "\033[1;33m"
+#define INFO_FONT	      "\033[1;37m"
+#define TERM_GREEN_FONT   "\033[1;32m"
+#define INVALID_LOG_FONT  "\033[1;34m"
 
 void DebugLog(DLOG_TYPE type, const char* string, err_loc location);
 
